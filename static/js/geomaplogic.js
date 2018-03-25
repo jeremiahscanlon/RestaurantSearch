3db27d5c9947f4d1ea641d213f3c57579f5a7c19
aparnapotdar <Aparna.p.potdar@gmail.com>
2018-03-25 01:46:15 -0400

    Add files via upload
    changes to add restaurants of 6 cuisine types; with custom marker for each type; popup with info and tomato link

--- static/js/geomaplogic.js
+++ static/js/geomaplogic.js
@@ -7,6 +7,36 @@ var myMap = L.map("map", {
   zoom: 13
 });
 
+var myIcon1 = L.icon({
+    iconUrl: '/static/css/american.png',
+    iconSize: [35, 35]
+});
+
+var myIcon2 = L.icon({
+    iconUrl: '/static/css/indian.png',
+    iconSize: [38, 35]
+});
+
+var myIcon3 = L.icon({
+    iconUrl: '/static/css/chinese.png',
+    iconSize: [35, 35]
+});
+
+var myIcon4 = L.icon({
+    iconUrl: '/static/css/mexican.png',
+    iconSize: [35, 35]
+});
+
+var myIcon5 = L.icon({
+    iconUrl: '/static/css/italian.png',
+    iconSize: [35, 35]
+});
+
+var myIcon6 = L.icon({
+    iconUrl: '/static/css/french.png',
+    iconSize: [35, 35]
+});
+
 // Adding tile layer to the map
 L.tileLayer(mapbox).addTo(myMap);
 
@@ -28,13 +58,13 @@ L.tileLayer(mapbox).addTo(myMap);
 
 //  }
 
-
+//cuisine ids for american, indian, chinese, mexican and italian french
+//cuisines=["1","148","25","73","55","45"]
 var zomato_url = "https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&cuisines=";
-var cuisine = "1";
 var zomato_key="&apikey=8b5af15b6d0dbad77cfa61ee84694638";
 
-// Assembling API query URL
-var url = zomato_url + cuisine+ zomato_key;
+//American
+var url = zomato_url + "1"+ zomato_key;
 
 // Grabbing the data with d3..
 d3.json(url, function(response) {
@@ -53,13 +83,177 @@ console.log(restaurant);
     if (restaurant) {
 
       // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude])
-        .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+      markers.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon1})
+        .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+        //.bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+    }
+//<a href="/map">Map</a>
+  }
+  // Add our marker cluster layer to the map
+ myMap.addLayer(markers);
+});
+
+//Indian
+var url = zomato_url + "148"+ zomato_key;
+
+// Grabbing the data with d3..
+d3.json(url, function(response) {
+
+  // Creating a new marker cluster group
+  var markers2 = L.markerClusterGroup();
+  console.log(response.restaurants.length);
+  //console.log("restaurant length "+string(response.restaurants.length));
+
+  // Loop through our data...
+  for (var i = 0; i < response.restaurants.length; i++) {
+    // set the data location property to a variable
+    var restaurant = response.restaurants[i].restaurant;
+console.log(restaurant);
+    // If the data has a location property...
+    if (restaurant) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers2.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon2})
+      .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+        //.bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
     }
 
   }
-
+ 
 
   // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
+  myMap.addLayer(markers2);
 });
+
+//chinese
+var url = zomato_url + "73"+ zomato_key;
+
+// Grabbing the data with d3..
+d3.json(url, function(response) {
+
+  // Creating a new marker cluster group
+  var markers3 = L.markerClusterGroup();
+  console.log(response.restaurants.length);
+  //console.log("restaurant length "+string(response.restaurants.length));
+
+  // Loop through our data...
+  for (var i = 0; i < response.restaurants.length; i++) {
+    // set the data location property to a variable
+    var restaurant = response.restaurants[i].restaurant;
+console.log(restaurant);
+    // If the data has a location property...
+    if (restaurant) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers3.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon3})
+      .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+      //  .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+    }
+
+  }
+ 
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers3);
+});
+
+
+//mexican
+var url = zomato_url + "73"+ zomato_key;
+
+// Grabbing the data with d3..
+d3.json(url, function(response) {
+
+  // Creating a new marker cluster group
+  var markers4 = L.markerClusterGroup();
+  console.log(response.restaurants.length);
+  //console.log("restaurant length "+string(response.restaurants.length));
+
+  // Loop through our data...
+  for (var i = 0; i < response.restaurants.length; i++) {
+    // set the data location property to a variable
+    var restaurant = response.restaurants[i].restaurant;
+console.log(restaurant);
+    // If the data has a location property...
+    if (restaurant) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers4.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon4})
+      .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+      //  .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+    }
+
+  }
+ 
+
+  // Add our marker cluster layer to the map
+  console.log("adding mexican");
+  myMap.addLayer(markers4);
+});
+
+//italian
+var url = zomato_url + "55"+ zomato_key;
+
+// Grabbing the data with d3..
+d3.json(url, function(response) {
+
+  // Creating a new marker cluster group
+  var markers5 = L.markerClusterGroup();
+  console.log(response.restaurants.length);
+  //console.log("restaurant length "+string(response.restaurants.length));
+
+  // Loop through our data...
+  for (var i = 0; i < response.restaurants.length; i++) {
+    // set the data location property to a variable
+    var restaurant = response.restaurants[i].restaurant;
+console.log(restaurant);
+    // If the data has a location property...
+    if (restaurant) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers5.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon5})
+      .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+      //  .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+    }
+
+  }
+ 
+
+  // Add our marker cluster layer to the map
+  console.log("adding italian");
+  myMap.addLayer(markers5);
+});
+
+//french
+var url = zomato_url + "45"+ zomato_key;
+
+// Grabbing the data with d3..
+d3.json(url, function(response) {
+
+  // Creating a new marker cluster group
+  var markers6 = L.markerClusterGroup();
+  console.log(response.restaurants.length);
+  //console.log("restaurant length "+string(response.restaurants.length));
+
+  // Loop through our data...
+  for (var i = 0; i < response.restaurants.length; i++) {
+    // set the data location property to a variable
+    var restaurant = response.restaurants[i].restaurant;
+console.log(restaurant);
+    // If the data has a location property...
+    if (restaurant) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers6.addLayer(L.marker([restaurant.location.latitude, restaurant.location.longitude], {icon: myIcon6})
+      .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" +  "<a href='"+ restaurant.url+"'>Zomato reviews url</a>"));
+      //  .bindPopup(restaurant.name + "<hr>" + restaurant.location.address+ "<hr>" + restaurant.cuisines + "<hr>" + "Avg Cost for 2: $" +restaurant.average_cost_for_two + "<hr>" + "Zomato reviews url: "+ restaurant.url));
+    }
+
+  }
+ 
+
+  // Add our marker cluster layer to the map
+  console.log("adding french");
+  myMap.addLayer(markers6);
+});
+
