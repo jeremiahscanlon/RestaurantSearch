8a40cae2d2cfd9ce36c51972b3012c9f33cef0f7
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2018-03-27 12:12:13 -0400

    removed access to pymongo

--- restaurantsearch/app.py
+++ restaurantsearch/app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, jsonify, redirect
-from flask_pymongo import PyMongo
-#import scrape_craigslist
 
 app = Flask(__name__)
-
-mongo = PyMongo(app)
-
 
 @app.route("/")
 def index():
@@ -26,17 +21,6 @@ def burst():
 def flare():
     #listings = mongo.db.listings.find_one()
     return render_template("flare.html")
-
-@app.route("/scrape")
-def scrape():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update(
-        {},
-        listings_data,
-        upsert=True
-    )
-    return redirect("http://localhost:5000/", code=302)
 
 
 if __name__ == "__main__":
